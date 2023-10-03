730ab2a117fb6c11e9c7cce0dd9b132ea559200c
Sans23410 <89539606+Sans23410@users.noreply.github.com>
2023-10-03 20:27:32 +0300

    Add files via upload

---
+++ commands/core/help.js
@@ -0,0 +1,21 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'help',
+    description: "All the commands this bot has!",
+    showHelp: false,
+
+    execute({ client, inter }) {
+        const commands = client.commands.filter(x => x.showHelp !== false);
+
+        const embed = new EmbedBuilder()
+        .setColor('#ff0000')
+        .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true }) })
+        .setDescription('Currently in testing, I will start working on this bot only in a year, because I am going to serve.')
+        .addFields([ { name: `Enabled - ${commands.size}`, value: commands.map(x => `\`${x.name}\``).join(' | ') } ])
+        .setTimestamp()
+        .setFooter({ text: 'Music comes first - Made with heart by Utsy/xaner33 ❤️', iconURL: inter.member.avatarURL({ dynamic: true })});
+
+        inter.editReply({ embeds: [embed] });
+    },
+};
---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    app: {
+        token: MTE1Mjc0NjIyMTY1NjYwNDY3Mg.Gfdj-0.B2kuyjtcWgikan0LjTUvFXjFbtchvSvK9CvTeE
+        playing: 'by .utsy/tochonemizix ❤️',
+        global: true,
+        guild: '638353358239039488',
+        ExtraMessages: false,
+        loopMessage: false,
+
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: '',
+            commands: []
+        },
+        maxVol: 100,
+        spotifyBridge: true,
+        volume: 75,
+        leaveOnEmpty: true,
+        leaveOnEmptyCooldown: 30000,
+        leaveOnEnd: true,
+        leaveOnEndCooldown: 30000,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
